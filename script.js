5bd5d1d0559aaf6b627d54cc76c95bd015b5b03b
Lu69as <lukasokken@gmail.com>
2024-09-27 14:38:31 +0200

    Big styling update wich should look better soon

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 if (localStorage.getItem("color") == "moon")
     document.body.classList.add("dark-mode");
 
-if (window.location.href.includes("no|en"))
-    document.body.classList.add("english");
-
-document.querySelectorAll("nav .colorMode svg").forEach((e) => {
-    e.addEventListener("click", () => { 
-        document.body.classList.toggle("dark-mode");
-        localStorage.setItem("color", e.classList[0])
-    })
-})
+setTimeout(() => {
+    if (window.location.href.includes("no|en") || document.querySelector("#about > p").innerHTML[0, 1] == "f")
+        document.body.classList.add("english");
+}, 1000);
 
 document.querySelectorAll("nav .lang a").forEach((e) => {
     e.addEventListener("click", () => { setTimeout(() => { location.reload(); }, 50); })
+})
+
+document.querySelectorAll("nav .colorMode svg").forEach((e) => {
+    e.addEventListener("click", () => { 
+        if (e.classList[0] == "moon") {
+            document.body.classList.add("dark-mode");
+            localStorage.setItem("color", "moon");
+        }
+        else {
+            document.body.classList.remove("dark-mode");
+            localStorage.setItem("color", "sun");
+        }
+    })
 })
 
 
@@ -34,38 +42,3 @@ document.addEventListener("scroll", () => {
     })
 })
 document.querySelector("nav a.home").classList.add("active");
-
-
-document.querySelectorAll("#portfolio h1 div").forEach((e) => {
-    e.addEventListener("click", () => {
-        let items = document.querySelectorAll("#portfolio .container .item");
-        let currentprop = getComputedStyle(items[0]).getPropertyValue("--itemPage");
-
-        if (e.classList.toString().includes("next"))
-            currentprop = currentprop >= items.length - getComputedStyle(items[0].parentElement)
-                .getPropertyValue("--items-fit") ? 0 : currentprop - 0 + 1;
-
-        else
-            currentprop = currentprop < 1 ? items.length - getComputedStyle(items[0].parentElement)
-                .getPropertyValue("--items-fit") : currentprop - 1;
-        
-        items.forEach((i) => i.style.setProperty("--itemPage", currentprop))
-    })
-
-    e.addEventListener("mouseenter", () => {
-        e.classList.add("pastNextArrow");
-
-        e.addEventListener("animationend", () => {
-            e.classList.remove("pastNextArrow");
-        })
-    })
-})
-
-window.onresize = () => {
-    document.querySelector("#portfolio .container").style.setProperty("--items-fit", 
-        Math.floor(document.querySelector("#portfolio .container").offsetWidth / 335)
-    )
-}
-document.querySelector("#portfolio .container").style.setProperty("--items-fit", 
-    Math.floor((window.outerWidth - 300 - 35 * 8 - 335 * document.querySelector("#portfolio .container").style.getPropertyValue("--items-fit")) / 300)
-)
